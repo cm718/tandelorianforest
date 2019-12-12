c7ef87cc750717b8c053d22a5bb462381ba3670a
cm718 <cm718@icloud.com>
2019-12-12 13:20:09 -0600

    added state and props to card

--- src/components/card.js
+++ src/components/card.js
@@ -1,41 +1,16 @@
 import React from 'react'
-import image from '../images/gatsby-icon.png'
+import image from '../images/bonsai.svg'
 
-export default function card() {
+export default function card(props) {
+    console.log(props);
     return (
         <div className='flex-row justify-around'>
              <div className="max-w-sm rounded overflow-hidden shadow-lg">
                 <div className="px-6 py-4 text-center">
-                <div className="font-bold text-xl mb-2">The Spider Plant</div>
-                <p className="text-gray-700 text-base">Water me every 7 days or I get thirsty...</p>
+                <div className="font-bold text-xl mb-2">{props.name}</div>
+                <p className="text-gray-700 text-base">Water me every {props.water} or I get thirsty...</p>
                 </div>
-                <img className="w-1/2 m-auto" src={image} alt="Sunset in the mountains" />
-                <div className="px-6 py-4 flex justify-center">
-                    <span className="
-                        inline-block bg-gray-200 
-                        rounded-full px-3 py-1 text-sm 
-                        font-semibold text-gray-700 mr-2">Water me</span>
-                </div>
-        </div>
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <div className="px-6 py-4 text-center">
-                <div className="font-bold text-xl mb-2">The Big Green</div>
-                <p className="text-gray-700 text-base">Water me every 7 days or I get thirsty...</p>
-                </div>
-                <img className="w-1/2 m-auto" src={image} alt="Sunset in the mountains" />
-                <div className="px-6 py-4 flex justify-center">
-                    <span className="
-                        inline-block bg-gray-200 
-                        rounded-full px-3 py-1 text-sm 
-                        font-semibold text-gray-700 mr-2">Water me</span>
-                </div>
-        </div>
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <div className="px-6 py-4 text-center">
-                <div className="font-bold text-xl mb-2">Between 2 Ferns</div>
-                <p className="text-gray-700 text-base">Water me every 7 days or I get thirsty...</p>
-                </div>
-                <img className="w-1/2 m-auto" src={image} alt="Sunset in the mountains" />
+                <img className="w-1/3 m-auto" src={image} alt="bonsai" />
                 <div className="px-6 py-4 flex justify-center">
                     <span className="
                         inline-block bg-gray-200 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import React, { Fragment } from "react"
 import Card from "../components/card"
+import plants from '../data/data.json'
 
-const IndexPage = () => (
-  <Fragment>
-    <Card />
-  </Fragment>
-)
+class IndexPage extends React.Component {
+  state={
+    plants,
+    month: "December",
+    date: 16
+  }
+  render(){
+    // console.log(this.state); 
+    const month = this.state.month;
+    const date = this.state.date;
+    return(
+      <Fragment className="m-8">
+         <h3 className='text-center'>Today is {month} {date}</h3>
+         {this.state.plants.map(plant => 
+          <Card key={plant.id} name={plant.name} water={plant.water_after}/>
+          )}
+          <div>Icons made by <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </Fragment>
+    )
+  }
+}
 
 export default IndexPage
