aedf241201b6ad695013a4a5872c19a4f3fabf2f
cm718 <cm718@icloud.com>
2019-12-12 19:03:58 -0600

    updated font color and weight

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,48 @@
 import React from "react"
 import Card from "../components/card"
-import plants from '../data/data.json'
+import plants from "../data/data.json"
 
 class IndexPage extends React.Component {
-  state={
+  state = {
     plants,
-    month: "December",
-    date: 16
+    date: 16,
   }
-  render(){
-    // console.log(this.state); 
-    const month = this.state.month;
-    const date = this.state.date;
-    return(
-      <div className="m-8">
-         <h3 className='text-center'>Plants to water on {month} {date}</h3>
-         <div className='flex-row flex flex-wrap justify-around'>
-            {this.state.plants.map(plant => 
-              <Card key={plant.id} src={plant.src} name={plant.name} water={plant.water_after}/>
-              )}
-         </div>
-          <div className='text-center mt-5' >Icons made by <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+  render() {
+    // console.log(this.state);
+    const date = this.state.date
+    return (
+      <div className="m-8 mx-auto container">
+        <h4 className="font-black text-center text-green-600">
+          Plants to water on 12/{date}
+        </h4>
+        <div
+          className="
+          flex-row 
+          flex flex-wrap 
+          justify-around"
+        >
+          {this.state.plants.map(plant => (
+            <Card
+              key={plant.id}
+              src={plant.src}
+              name={plant.name}
+              water={plant.water_after}
+            />
+          ))}
+        </div>
+        <h6 className="text-center mt-8">
+          Icons made by{" "}
+          <a
+            href="https://www.flaticon.com/authors/ultimatearm"
+            title="ultimatearm"
+          >
+            ultimatearm
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </h6>
       </div>
     )
   }
