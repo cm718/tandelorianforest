27dfd3614fda1db99b85e810528d81a7e621eee8
cm718 <cm718@icloud.com>
2019-12-12 23:58:17 -0600

    destructured props and ordered media queries to mobile first

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,22 @@
 import React from "react"
 
-const Card = props => {
+const Card = ({ water, src, name }) => {
   return (
     // card body
-    <div className="xl:w-1/4 md:w-1/3 sm:w-1/2 rounded-lg shadow-lg my-5">
+    <div className="sm:w-1/2 md:w-1/3 xl:w-1/4 rounded-lg shadow-lg my-5 p-1">
       {/* div for text including the name and the subheading to water it */}
       <div className="px-6 py-4 text-center">
-        <div className="font-bold text-xl mb-1">{props.name}</div>
+        <div className="font-bold text-xl mb-1">{name}</div>
         <p className="text-gray-700 text-base">
-          Water me every <span className='font-black text-green-600'>{props.water}</span> or I get thirsty...
+          Water me every <span className='font-black text-green-600'>{water}</span> or I get thirsty...
         </p>
       </div>
       {/* image tag with styles and src */}
       <img
         style={{ width: "125px", height: "125px" }}
         className="mx-auto"
-        src={props.src}
-        alt={props.name}
+        src={src}
+        alt={name}
       />
     </div>
   )
