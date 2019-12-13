5cbead02e4f8ee52f97def11e8a558018f4fdb5a
cm718 <cm718@icloud.com>
2019-12-12 19:05:48 -0600

    added span for water day color and removed lower span

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react"
 
-export default function card(props) {
-    return (
-      
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <div className="px-6 py-4 text-center">
-                <div className="font-bold text-xl mb-2">{props.name}</div>
-                <p className="text-gray-700 text-base">Water me every {props.water} or I get thirsty...</p>
-                </div>
-                <img className="w-1/3 m-auto my-5" src={props.src} alt={props.name} />
-                <div className="px-6 py-4 flex justify-center">
-                    <span className="
-                        inline-block bg-gray-200 
-                        rounded-full px-3 py-1 text-sm 
-                        font-semibold text-gray-700">Water me</span>
-                </div>
-        </div>
-    )
+const Card = props => {
+  return (
+    <div className="lg:w-1/4 md:1/2 s:1 rounded-lg shadow-lg">
+      <div className="px-6 py-4 text-center">
+        <div className="font-bold text-xl mb-1">{props.name}</div>
+        <p className="text-gray-700 text-base">
+          Water me every <span className='font-black text-green-600'>{props.water}</span> or I get thirsty...
+        </p>
+      </div>
+      <img
+        style={{ width: "150px" }}
+        className="mx-auto"
+        src={props.src}
+        alt={props.name}
+      />
+    </div>
+  )
 }
+
+export default Card
