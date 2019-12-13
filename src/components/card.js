486a9b82772dc78bafe4849de94b154beaa680b4
cm718 <cm718@icloud.com>
2019-12-12 21:44:26 -0600

    added responsiveness and sizing for card img

--- src/components/card.js
+++ src/components/card.js
@@ -2,15 +2,18 @@ import React from "react"
 
 const Card = props => {
   return (
-    <div className="lg:w-1/4 md:1/2 s:1 rounded-lg shadow-lg">
+    // card body
+    <div className="xl:w-1/4 md:w-1/3 sm:w-1/2 rounded-lg shadow-lg">
+      {/* div for text including the name and the subheading to water it */}
       <div className="px-6 py-4 text-center">
         <div className="font-bold text-xl mb-1">{props.name}</div>
         <p className="text-gray-700 text-base">
           Water me every <span className='font-black text-green-600'>{props.water}</span> or I get thirsty...
         </p>
       </div>
+      {/* image tag with styles and src */}
       <img
-        style={{ width: "150px" }}
+        style={{ width: "125px", height: "125px" }}
         className="mx-auto"
         src={props.src}
         alt={props.name}
