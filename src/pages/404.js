dff1a5c29fa3c86d5cc06ec697e9bd43a458a9a9
cm718 <cm718@icloud.com>
2019-12-13 15:42:47 -0600

    removed gatsy stock code

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,10 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <div>
+    <h1 className='text-center text-red-700'>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness. To cheer you up why don't you water some plants instead.</p>
+  </div>
 )
 
 export default NotFoundPage
