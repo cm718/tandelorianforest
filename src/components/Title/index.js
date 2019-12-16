421ee180aafb1bc03c7f5d51c4285242deedca1c
cm718 <cm718@icloud.com>
2019-12-16 15:24:25 -0600

    Put day in a separate div to raise it up

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,11 +3,12 @@ import React from "react"
 const Title = ({day, month, date}) => {
   return (
     <div>
-      <div className="my-10 text-xl font-black text-center text-green-600">
-        {day}, {month} {date}
-      </div>
-      <div className="mb-4 text-lg font-black text-center text-green-500">
-        Plants to water today
+      <div className="my-10 text-3xl font-black text-center text-green-600">
+        {day} 
+        <div>
+
+        {month} {date}
+        </div>
       </div>
     </div>
   )
