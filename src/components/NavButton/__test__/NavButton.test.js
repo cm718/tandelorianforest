6af970a0c062263f7177eb178bb8ddabc76c2391
cm718 <cm718@icloud.com>
2019-12-16 01:54:21 -0600

    Wrote snapshot test for button and update Jest devDependencies

--- src/components/NavButton/__test__/NavButton.test.js
+++ src/components/NavButton/__test__/NavButton.test.js
@@ -1,4 +1,19 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import React from "react"
+import { render } from "@testing-library/react"
+import NavButton from "../index.js"
 
-console.log('testing this thang')
+describe("Navigation button renders correctly", () => {
+  test("correct button name is passed through props", () => {
+    const { container } = render(<NavButton name="Next" />)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button
+          class=" bg-green-600  text-white font-semibold  py-1 px-2 w-full md:w-32   border border-green-500 hover:bg-green-500  focus:outline-none focus-shadow-outline  rounded"
+        >
+          Next
+           Day
+        </button>
+      </div>
+    `)
+  })
+})
