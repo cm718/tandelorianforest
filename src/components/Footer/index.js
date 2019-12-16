2b78d29efff2cd7b94a7bff3e2628028de89237a
cm718 <cm718@icloud.com>
2019-12-16 15:21:31 -0600

    Updated footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <h6 className="text-center hover:text-green-700 text-gray-300 mt-8 mb-2 text-xs">
+    <h6 className="text-center hover:text-green-700 text-gray-300 mt-8 mb-2 text-xs bottom-0">
       Icons made by{" "}
       <a
         href="https://www.flaticon.com/authors/ultimatearm"
